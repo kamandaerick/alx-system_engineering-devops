49574ad8effa02a101ed1aa1d4e6efdae5dedf7d
Kamanda <erickckamanda@gmail.com>
2023-12-22 10:21:13 -0800

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,8 +4,14 @@ package { 'python3-pip':
   ensure => installed,
 }
 
+package { 'Werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
+  require  => [Package['python3-pip'], Package['Werkzeug']],
 }
