2e61add54d13496c409cc0e0983a16fb072646e9
Kamanda <erickckamanda@gmail.com>
2023-12-22 10:12:00 -0800

    Task 2

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# install flas from pip3
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
