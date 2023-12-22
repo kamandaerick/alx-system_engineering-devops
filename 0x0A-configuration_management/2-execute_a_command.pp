39933c110584404d3d5926f5b80542cb3ea22c5f
Kamanda <erickckamanda@gmail.com>
2023-12-22 11:11:56 -0800

    Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,11 @@
 exec { 'killmenow':
-  command => 'pkill killmenow',
-  path    => ['/bin', '/usr/bin'],
+  command     => 'pkill -f killmenow',
+  path        => '/usr/bin:/bin',
+  refreshonly => true,
+}
+
+file { '/path/to/killmenow':
+  ensure  => present,
+  content => "#!/bin/bash\nwhile [[ true ]]; do sleep 2; done\n",
+  mode    => '0755',
 }
