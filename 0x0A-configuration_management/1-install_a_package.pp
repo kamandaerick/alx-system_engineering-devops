873a837773a6b7c5565e4ed44e6cbc768db53660
Kamanda <erickckamanda@gmail.com>
2024-06-28 08:39:43 -0700

    Install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# This manifest installs Flask version 2.1.0 using pip3.
-
-package { 'Flask':
-  ensure => '2.1.0',
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
