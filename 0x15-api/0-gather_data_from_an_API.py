3b1eb79011d8f527d7f4c4cedf9c56cd741bf951
Kamanda <erickckamanda@gmail.com>
2024-07-29 05:48:28 -0700

    Get and return employee's todo list

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Get and return information about an employee's todo"""
+import requests
+import sys
+import json
+
+# Get the user ID from command line arguments
+user_id = sys.argv[1]
+
+# Define URLs for user details and user tasks
+tasks_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+name_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+
+# Fetch user details
+name_response = requests.get(name_url)
+names = name_response.json()
+name = names['name']
+
+# Fetch user tasks
+response = requests.get(tasks_url)
+
+if response.status_code == 200:
+    tasks = response.json()
+    completed_count = 0
+    uncompleted_count = 0
+    completed_titles = []
+
+    # Process each task
+    for task in tasks:
+        if task["completed"]:
+            completed_count += 1
+            completed_titles.append(task["title"])
+        else:
+            uncompleted_count += 1
+
+    # Print results
+    print(f"Employee {name} is done with tasks \
+            ({completed_count}/{completed_count + uncompleted_count}):")
+    for title in completed_titles:
+        print(f"     {title}")
+
+else:
+    print(f"Failed to retrieve data: {response.status_code}")
