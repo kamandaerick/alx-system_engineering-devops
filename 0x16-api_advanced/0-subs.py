a45e2a6fab29d804abcd45a5f387eff8fd79e0b9
Kamanda <erickckamanda@gmail.com>
2024-08-13 10:55:59 -0700

    Get the number of subscribers of a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+Return the number of subscribers for a given subreddit
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """
+    Get the number of subscribers of a subreddit
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (kamanda)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    user_agent = {'User-agent': 'kamanda'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
